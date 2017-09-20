73171b01d5cdbb70e1110336fd54d3b87b2012f2
chenweiqi <chenweiqi@yanxiu.com>
2017-09-20 17:00:41 +0800

    remove setvisibletouser

--- app/src/main/java/com/yanxiu/gphone/faceshow/homepage/NaviFragmentFactory.java
+++ app/src/main/java/com/yanxiu/gphone/faceshow/homepage/NaviFragmentFactory.java
@@ -63,19 +63,15 @@ public class NaviFragmentFactory {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         if (mCurrItem == 0 && mHomeFragment != null) {
             transaction.hide(mHomeFragment);
-            mHomeFragment.setUserVisibleHint(false);
         }
         if (mCurrItem == 1 && mNoticeFragment != null) {
             transaction.hide(mNoticeFragment);
-            mNoticeFragment.setUserVisibleHint(false);
         }
         if (mCurrItem == 2 && mClassCircleFragment != null) {
             transaction.hide(mClassCircleFragment);
-            mClassCircleFragment.setUserVisibleHint(false);
         }
         if (mCurrItem == 3 && mMyFragment != null) {
             transaction.hide(mMyFragment);
-            mMyFragment.setUserVisibleHint(false);
         }
         mCurrItem = index;
         switch (mCurrItem) {
@@ -83,40 +79,32 @@ public class NaviFragmentFactory {
                 if (mHomeFragment == null) {
                     mHomeFragment = new HomeFragment();
                     transaction.add(R.id.content_main, mHomeFragment);
-                    mHomeFragment.setUserVisibleHint(true);
                 } else {
                     transaction.show(mHomeFragment);
-                    mHomeFragment.setUserVisibleHint(true);
                 }
                 break;
             case 1:
                 if (mNoticeFragment == null) {
                     mNoticeFragment = new NoticeFragment();
                     transaction.add(R.id.content_main, mNoticeFragment);
-                    mNoticeFragment.setUserVisibleHint(true);
                 } else {
                     transaction.show(mNoticeFragment);
-                    mNoticeFragment.setUserVisibleHint(true);
                 }
                 break;
             case 2:
                 if (mClassCircleFragment == null) {
                     mClassCircleFragment = new ClassCircleFragment();
                     transaction.add(R.id.content_main, mClassCircleFragment);
-                    mClassCircleFragment.setUserVisibleHint(true);
                 } else {
                     transaction.show(mClassCircleFragment);
-                    mClassCircleFragment.setUserVisibleHint(true);
                 }
                 break;
             case 3:
                 if (mMyFragment == null) {
                     mMyFragment = new MyFragment();
                     transaction.add(R.id.content_main, mMyFragment);
-                    mMyFragment.setUserVisibleHint(true);
                 } else {
                     transaction.show(mMyFragment);
-                    mMyFragment.setUserVisibleHint(true);
                 }
                 break;
         }
