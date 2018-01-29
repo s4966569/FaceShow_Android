f882e3bd28d1d2a2aee25b49464fa4fb40ee58a7
XZZ <847651040@qq.com>
2018-01-29 14:48:14 +0800

    修改web显示样式

--- app/src/main/java/com/yanxiu/gphone/faceshow/webView/FaceShowWebActivity.java
+++ app/src/main/java/com/yanxiu/gphone/faceshow/webView/FaceShowWebActivity.java
@@ -3,9 +3,12 @@ package com.yanxiu.gphone.faceshow.webView;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.just.agentweb.AgentWeb;
@@ -14,6 +17,7 @@ import com.yanxiu.gphone.faceshow.R;
 import com.yanxiu.gphone.faceshow.base.FaceShowBaseActivity;
 import com.yanxiu.gphone.faceshow.db.SpManager;
 import com.yanxiu.gphone.faceshow.http.main.GetToolsResponse;
+import com.yanxiu.gphone.faceshow.util.ScreenUtils;
 
 
 import butterknife.BindView;
@@ -22,48 +26,57 @@ import butterknife.OnClick;
 
 /**
  * 使用了三方架包 AgentWeb
+ *
  * @author frc
  */
 public class FaceShowWebActivity extends FaceShowBaseActivity {
 
-    @BindView(R.id.title_layout_left_img)
-    ImageView mTitleLayoutLeftImg;
-    @BindView(R.id.title_layout_title)
-    TextView mTitleLayoutTitle;
     @BindView(R.id.ll_root)
-    LinearLayout mLlRoot;
+    RelativeLayout mLlRoot;
     GetToolsResponse data;
     AgentWeb mAgentWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 隐藏标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_face_show_web_layout);
         ButterKnife.bind(this);
         data = (GetToolsResponse) getIntent().getSerializableExtra("data");
 
-        String url = data.getData().getTools().get(0).getEventObj().getContent() ;
-        if (url.contains("?")){
-            url=url+ "&classId=" + SpManager.getUserInfo().getClassId() + "&token=" + SpManager.getToken()+"&_="+System.currentTimeMillis();
-        }else {
-            url=url+ "?classId=" + SpManager.getUserInfo().getClassId() + "&token=" + SpManager.getToken()+"&_="+System.currentTimeMillis();
+        String url = data.getData().getTools().get(0).getEventObj().getContent();
+        if (url.contains("?")) {
+            url = url + "&classId=" + SpManager.getUserInfo().getClassId() + "&token=" + SpManager.getToken() + "&_=" + System.currentTimeMillis();
+        } else {
+            url = url + "?classId=" + SpManager.getUserInfo().getClassId() + "&token=" + SpManager.getToken() + "&_=" + System.currentTimeMillis();
         }
 
-        mTitleLayoutLeftImg.setVisibility(View.VISIBLE);
 
         mAgentWeb = AgentWeb.with(this)
-                .setAgentWebParent(mLlRoot, new LinearLayout.LayoutParams(-1, -1))
+                .setAgentWebParent(mLlRoot, new RelativeLayout.LayoutParams(-1, -1))
                 .useDefaultIndicator()
                 .defaultProgressBarColor()
-                .setReceivedTitleCallback(new ChromeClientCallbackManager.ReceivedTitleCallback() {
-                    @Override
-                    public void onReceivedTitle(WebView view, String title) {
-                        mTitleLayoutTitle.setText(title);
-                    }
-                })
                 .createAgentWeb()
                 .ready()
                 .go(url);
+        ImageView backImage = new ImageView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams((int) ScreenUtils.dpToPx(this, 30),(int) ScreenUtils.dpToPx(this, 30));
+        layoutParams.setMargins((int) ScreenUtils.dpToPx(this, 5), (int) ScreenUtils.dpToPx(this, 10), 0, 0);
+        backImage.setLayoutParams(layoutParams);
+        backImage.setBackgroundResource(R.drawable.selector_back);
+        mLlRoot.addView(backImage);
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FaceShowWebActivity.this.finish();
+            }
+        });
+
+
     }
 
     @Override
@@ -75,10 +88,6 @@ public class FaceShowWebActivity extends FaceShowBaseActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    @OnClick(R.id.title_layout_left_img)
-    public void onViewClicked() {
-        this.finish();
-    }
 
     @Override
     protected void onResume() {
