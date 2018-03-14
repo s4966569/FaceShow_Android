5081944948b6c596c1252c2c9a133ccffdee152b
zhuxiaolong <zhuxiaolong@yanxiu.com>
2018-03-14 18:07:11 +0800

    部分UI 比对与调整

--- app/src/main/java/com/yanxiu/gphone/faceshow/qrsignup/fragment/SetPasswordFragment.java
+++ app/src/main/java/com/yanxiu/gphone/faceshow/qrsignup/fragment/SetPasswordFragment.java
@@ -323,6 +323,7 @@ public class SetPasswordFragment extends FaceShowBaseFragment {
                 alertDialog.dismiss();
             }
         });
+
         alertDialog = builder.create();
     }
 }
